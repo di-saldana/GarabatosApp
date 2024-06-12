51737b2b206939eb40a818a1d205a6ffcd54d57b
di-saldana <dsl42@alu.ua.es>
2024-06-12 23:04:46 +0200

    Initial Commit

---
+++ Garabatos/GarabatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarabatosApp.swift
+//  Garabatos
+//
+//  Created by Dianelys Saldaña on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GarabatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
